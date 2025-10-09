fa972e2ec5c893e8f51a20b6fb2779559abf76a4
pragnakalpdev65 <pragnakalp.dev65@gmail.com>
2025-10-09 10:25:42 +0530

    Delete E_commerce_app directory

--- E_commerce_app/appurls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='home'), 
-    path('register/', views.RegisterView.as_view(), name='register'),  
-    path('login/',views.LoginView.as_view(),name='login'),
-]
